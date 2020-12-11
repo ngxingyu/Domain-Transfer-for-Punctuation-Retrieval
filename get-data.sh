b534547dd5e99e7d7c030578d7290a0739de5d63
Ng Xing Yu <nxingyu@intern-instance.us-central1-f.c.b-baker-street.internal>
2020-12-11 10:53:38 +0800

    fixed speakertag flaw

--- get-data.sh
+++ get-data.sh
@@ -1,6 +1,7 @@
 #Download zipfile from https://www.kaggle.com/miguelcorraljr/ted-ultimate-dataset
 cd ~/data/
-wget -O TedTalks.zip http://opus.nlpl.eu/download.php?f=TedTalks/v1/raw/en.zip
+#wget -O TedTalks.zip http://opus.nlpl.eu/download.php?f=TedTalks/v1/raw/en.zip
 wget -O OpenSubtitles.zip http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2016/raw/en.zip
-unzip TedTalks.zip
+#unzip TedTalks.zip
 unzip OpenSubtitles.zip
+bash ~/project/xml2csv.sh -i ./OpenSubtitles -o ./OpenSubtitles.csv
