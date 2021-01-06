61599bc2391f6c4f15870d3149a3ae8c0f9bbb42
Ng Xing Yu <nxingyu@intern-instance.us-central1-f.c.b-baker-street.internal>
2021-01-06 22:52:40 +0800

    update

--- text2aligned.py
+++ text2aligned.py
@@ -6,6 +6,20 @@ import argparse, os, csv
 from datasets import load_dataset
 import transformers
 
+class PunctuationDataset(torch.utils.data.Dataset):
+    def __init__(self, input_ids, attention_mask, labels):
+        self.input_ids = input_ids
+        self.attention_mask = attention_mask
+        self.labels = labels
+    def __getitem__(self, idx):
+        item = {'input_ids': torch.tensor(self.input_ids[idx],dtype=torch.long),
+        'attention_mask': torch.tensor(self.attention_mask[idx],dtype=torch.long),
+        'labels': torch.tensor(self.labels[idx],dtype=torch.long)
+        }
+        return item
+    def __len__(self):
+        return len(self.labels)
+
 def validate_file(f):
     if not os.path.exists(f):
         raise argparse.ArgumentTypeError("{0} does not exist".format(f))
@@ -20,7 +34,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-				
+
 tags=sorted(list('.?!,;:-—…'))
 tag2id = {tag: id+1 for id, tag in enumerate(tags)}
 id2tag = {id: tag for tag, id in tag2id.items()}
@@ -29,8 +43,8 @@ tokenizer = transformers.DistilBertTokenizerFast.from_pretrained('distilbert-bas
 def text2masks(n):
     def text2masks(text):
         '''Converts single paragraph of text into a list of words and corresponding punctuation based on the degree requested.'''
-        if n==0: 
-            refilter="(?<=[.?!,;:\-—… ])(?=[^.?!,;:\-—… ])|$" 
+        if n==0:
+            refilter="(?<=[.?!,;:\-—… ])(?=[^.?!,;:\-—… ])|$"
         else:
             refilter="[.?!,;:\-—…]{1,%d}(?= *[^.?!,;:\-—…]+|$)|(?<=[^.?!,;:\-—…]) +(?=[^.?!,;:\-—…])"%(n)
         word=re.split(refilter,text, flags=re.V1)
@@ -75,13 +89,13 @@ def encode_tags(encodings, docs, max_length, overlap):
             doc_id+=1
             label_offset=0
             print('.', end='')
-        doc_enc_labels = np.ones(len(doc_offset),dtype=int) * -100
+        doc_enc_labels = np.ones(len(doc_offset),dtype=int) * 0 #-100
         doc_enc_labels[0]=docs[doc_id][label_offset-1] # Set leading punctuation class
 #         print([id2tag[t] if t>0 else '' for t in docs[doc_id][label_offset:label_offset+len(doc_offset)]])
         arr_offset = np.array(doc_offset)
         arr_mask = (arr_offset[:,0] == 0) & (arr_offset[:,1] != 0) # Gives the labels that should be assigned punctuation
         doc_enc_labels[arr_mask] = docs[doc_id][label_offset:label_offset+sum(arr_mask)]
-        encoded_labels.append(doc_enc_labels)            
+        encoded_labels.append(doc_enc_labels)
         label_offset+=sum(arr_mask[:max_length-overlap-1])
     return encoded_labels
 
@@ -89,18 +103,13 @@ def process_dataset(dataset, split, max_length=128, overlap=63):
     data=dataset[split].map(chunk_examples_with_degree(0), batched=True, batch_size=max_length,remove_columns=dataset[split].column_names)
     encodings=tokenizer(data['texts'], is_split_into_words=True, return_offsets_mapping=True,
               return_overflowing_tokens=True, padding=True, truncation=True, max_length=max_length, stride=overlap)
-    
     labels=encode_tags(encodings, data['tags'], max_length, overlap)
     encodings.pop("offset_mapping")
     encodings.pop("overflow_to_sample_mapping")
-#     train_dataset = tf.data.Dataset.from_tensor_slices((
-#         dict(train_encodings),
-#         train_labels
-#     ))
-    return torch.utils.data.TensorDataset(torch.tensor(encodings['input_ids'],dtype=torch.long),
+    return PunctuationDataset(torch.tensor(encodings['input_ids'],dtype=torch.long),
         torch.tensor(encodings['attention_mask'],dtype=torch.long),
         torch.tensor(labels,dtype=torch.long))
-        
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Enter csv file location. Extracts the "talk_id" and "transcript" column from csv and preprocesses transcript to the format for sentence punctuation prediction')
@@ -110,13 +119,19 @@ if __name__ == "__main__":
                         help="max sequence length", default=128, type=int)
     parser.add_argument("-o", "--overlap", dest="overlap_length", required=False,
                         help="max sequence length", default=63, type=int)
+    parser.add_argument("-s", "--split", dest="splits", required=False,
+                        help="single split, train dev test if empty", default='', type=str)
 
     args = parser.parse_args()
-    for split in ['train', 'dev', 'test']:
+    if (args.splits==''):
+        splits=['train', 'dev', 'test']
+    else:
+        splits=[args.splits]
+    for split in splits:
         paths=os.path.splitext(args.filename)
         filename=paths[0]+'.'+split+paths[1]
         print(validate_file(filename+'.csv'))
         ted=load_dataset('csv',data_files={split:filename+'.csv'})
         dataset=process_dataset(ted,split,args.max_length,args.overlap_length)
         torch.save(dataset, filename+'.pt')
-		
+
