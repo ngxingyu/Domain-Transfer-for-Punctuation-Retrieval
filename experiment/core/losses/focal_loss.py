ca5862317b7d1f0e5c18935426976e650180da60
XYNG <ng.xingyu@dhs.sg>
2021-02-04 16:41:54 +0800

    updated focal loss

--- experiment/core/losses/focal_loss.py
+++ experiment/core/losses/focal_loss.py
@@ -2,29 +2,26 @@ import torch
 from torch import Tensor
 import torch.nn.functional as F
 from typing import Callable, Optional
-from torch.nn.modules.loss import _WeightedLoss
 
 __all__ = ['FocalLoss']
 
-class FocalLoss(_WeightedLoss):
+class FocalLoss(torch.nn.NLLLoss):
 
     __constants__ = ['gamma', 'reduction']
     gamma: int
 
     def __init__(self, weight: Optional[Tensor] = None, gamma=2, size_average=None, 
-                ignore_index: int = -100, reduce=None, 
-                 reduction: str = 'mean') -> None:         
+                ignore_index: int = -100, reduce=None, reduction: str = 'mean') -> None:         
         if weight is not None and not torch.is_tensor(weight):
             weight = torch.FloatTensor(weight)
-        super(FocalLoss, self).__init__(weight, size_average, reduce, reduction)
-        self.ignore_index = ignore_index
+        super(FocalLoss, self).__init__(weight, size_average, ignore_index, reduce, reduction)
         self.gamma = gamma
-    @snoop
-    def forward(self, logits: Tensor, labels: Tensor, loss_mask=None) -> Tensor:
 
+    def forward(self, logits: Tensor, labels: Tensor, loss_mask=None) -> Tensor:
         logits_flatten = torch.flatten(logits, start_dim=0, end_dim=-2)
         labels_flatten = torch.flatten(labels, start_dim=0, end_dim=-1)
-
+        self.num_classes=logits_flatten.shape[-1]
+        
         if loss_mask is not None:
             if loss_mask.dtype is not torch.bool:
                 loss_mask = loss_mask > 0.5
@@ -37,22 +34,17 @@ class FocalLoss(_WeightedLoss):
             labels_flatten = torch.argmax(logits, dim=-1)
 
         logits_flatten_soft =F.log_softmax(logits_flatten,-1)
-        target_one_hot=F.one_hot(labels_flatten,num_classes=logits_flatten_soft.shape[-1])
-        weight = torch.tensor([1.]*self.num_classes).type_as(logits) if self.weight is None else self.weight
+        ce = super().forward(logits_flatten_soft,labels_flatten)
+        all_rows = torch.arange(len(logits_flatten))
+        log_pt = logits_flatten_soft[all_rows, labels_flatten]
 
-        # p_t = torch.where(target_one_hot==1, logits_flatten_soft, 1-logits_flatten_soft)
-        p_t = logits_flatten_soft*target_one_hot
-        fl = - 1 * (1 - p_t) ** self.gamma * torch.log(p_t)
-        fl = torch.where(target == 1, fl * self.alpha, fl)
+        # compute focal term: (1 - pt)^gamma
+        pt = log_pt.exp()
+        focal_term = (1 - pt)**self.gamma
 
-        pt = torch.exp(-ce_loss)
-        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
-        return self._reduce(focal_loss)
-
-    def _reduce(self, x):
+        loss = focal_term * ce
         if self.reduction == 'mean':
-            return x.mean()
+            return loss.mean()
         elif self.reduction == 'sum':
-            return x.sum()
-        else:
-            return x
+            return loss.sum()
+        return loss
