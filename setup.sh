6d9171e54f96cac5f356c9b8161249a99222a9e4
ngxingyu <ng.xingyu@dhs.sg>
2021-02-10 01:31:21 +0000

    tidy readme

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash Miniconda3-latest-Linux-x86_64.sh
-# source ~/.bashrc
+sudo apt install screen
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
 conda create --name NLP python=3.8
 conda activate NLP
-pip install snoop nemo_toolkit[all]==1.0.0b2 transformers datasets hydra-core
+pip install snoop nemo_toolkit[all]==1.0.0b2 transformers datasets hydra-core git+https://github.com/pabloppp/pytorch-tools -U
 conda install -c conda-forge pytorch-lightning
 conda install pytorch cudatoolkit=10.1 -c pytorch
