30b0779da1dc7d8532f2ab88ff7c6dc167a1154e
XYNG <ng.xingyu@dhs.sg>
2021-02-10 11:51:21 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash Miniconda3-latest-Linux-x86_64.sh
-# source ~/.bashrc
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -f -p ~
+source ~/.bashrc
 conda create --name NLP python=3.8
 conda activate NLP
 pip install snoop nemo_toolkit[all]==1.0.0b2 transformers datasets hydra-core
