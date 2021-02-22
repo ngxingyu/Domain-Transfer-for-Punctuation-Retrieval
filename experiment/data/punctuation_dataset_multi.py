3484df587d6784671be23ee3670ce5b6e707a761
XYNG <ng.xingyu@dhs.sg>
2021-02-22 08:57:13 +0800

    update pad start end

--- experiment/data/punctuation_dataset_multi.py
+++ experiment/data/punctuation_dataset_multi.py
@@ -43,6 +43,7 @@ class PunctuationDomainDataset(IterableDataset):
         attach_label_to_end=None,
         no_space_label=None,
         manual_len=0,
+        pad_start_and_end=0,
     ):
         if not (os.path.exists(csv_file)):
             raise FileNotFoundError(
@@ -71,6 +72,7 @@ class PunctuationDomainDataset(IterableDataset):
         self.tmp_path=tmp_path
         self.attach_label_to_end=attach_label_to_end
         self.no_space_label=no_space_label
+        self.pad_start_and_end=pad_start_and_end
         if not (os.path.exists(self.target_file)):
             os.system(f"sed '1d' {self.csv_file} > {self.target_file}")
 
@@ -94,7 +96,7 @@ class PunctuationDomainDataset(IterableDataset):
         # b=np.minimum(l,a+self.max_seq_length*n)
         # batch=pd.DataFrame({'t':batch,'a':a,'b':b}).apply(lambda row: ' '.join(row.t.split()[row.a:row.b]),axis=1)
 
-        chunked=chunk_examples_with_degree(self.degree, self.punct_label_ids, self.label_map)(batch)
+        chunked=chunk_examples_with_degree(self.degree, self.punct_label_ids, self.label_map, self.pad_start_and_end)(batch)
         batched=chunk_to_len_batch(self.max_seq_length,self.tokenizer,chunked['texts'],chunked['tags'],self.labelled,attach_label_to_end=self.attach_label_to_end,no_space_label=self.no_space_label)
         num_samples=batched['labels'].shape[0]
         batched['domain']=self.domain*torch.ones(num_samples,1,dtype=torch.long)
@@ -167,6 +169,7 @@ class PunctuationDomainDatasets(IterableDataset):
                  attach_label_to_end=None,
                  manual_len:int=0,
                  no_space_label:int=None,
+                 pad_start_and_end:int=0,
                  ):
         worker_info = get_worker_info()
         self.num_workers=1 if worker_info is None else worker_info.num_workers
@@ -201,7 +204,8 @@ class PunctuationDomainDatasets(IterableDataset):
                     tmp_path=tmp_path,
                     attach_label_to_end=attach_label_to_end,
                     no_space_label=no_space_label,
-                    manual_len=manual_len,)
+                    manual_len=manual_len,
+                    pad_start_and_end=pad_start_and_end,)
             self.datasets.append(dataset)
             self.iterables.append(cycle(dataset))
             
@@ -217,7 +221,8 @@ class PunctuationDomainDatasets(IterableDataset):
                     tmp_path=tmp_path,
                     attach_label_to_end=attach_label_to_end,
                     no_space_label=no_space_label,
-                    manual_len=manual_len,)
+                    manual_len=manual_len,
+                    pad_start_and_end=pad_start_and_end,)
             self.datasets.append(dataset)
             self.iterables.append(cycle(dataset))
 
@@ -289,12 +294,23 @@ class PunctuationInferenceDataset(Dataset):
             "labels": NeuralType(('B', 'T'), ChannelType()),
         }
 
-    def __init__(self, tokenizer, queries: List[str], max_seq_length: int, punct_label_ids:Dict[str,int], label_map:Dict[str,str], num_samples:int=256, degree:int = 0, attach_label_to_end:bool=None,no_space_label=None,):
+    def __init__(self, 
+    tokenizer, 
+    queries: List[str], 
+    max_seq_length: int, 
+    punct_label_ids:Dict[str,int], 
+    label_map:Dict[str,str], 
+    num_samples:int=256, 
+    degree:int = 0, 
+    attach_label_to_end:bool=None,
+    no_space_label=None,
+    pad_start_and_end:int=0,
+    ):
         """ Initializes BertPunctuationInferDataset. """
         self.degree=degree
         self.punct_label_ids=punct_label_ids
         self.label_map = label_map
-        chunked=chunk_examples_with_degree(self.degree, self.punct_label_ids, self.label_map)(queries)
+        chunked=chunk_examples_with_degree(self.degree, self.punct_label_ids, self.label_map, self.pad_start_and_end)(queries)
         self.features = chunk_to_len_batch(max_seq_length, tokenizer,chunked['texts'],chunked['tags'],attach_label_to_end=attach_label_to_end,no_space_label=no_space_label,)
         self.attach_label_to_end=attach_label_to_end
         # self.all_input_ids = features['input_ids']
