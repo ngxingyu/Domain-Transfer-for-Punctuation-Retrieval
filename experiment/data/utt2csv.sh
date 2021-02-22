94ff24bf9991a8dc7986fd6b88f23fd7a9a73fb5
Xing Yu <ng.xingyu@dhs.sg>
2021-02-22 16:32:09 +0800

    create processutt, fix bash find directory problem

--- experiment/data/utt2csv.sh
+++ experiment/data/utt2csv.sh
@@ -6,10 +6,10 @@ echo "out $2"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo 'talk_id,transcript'> "$2"
 #echo "filenames, transcript" > opensubtitles.csv
-for folder in $1/*; do
-    for filename in $folder/*.dfl; do
+for folder in $1/sw*; do
+    for filename in $folder/*.utt; do
         echo $filename
-        python $DIR/processdff.py -i $filename -o $2
+        python $DIR/processutt.py -i $filename -o $2
         #echo "$filename, \"$transcript\"" >> opensubtitles.csv
         # break 0
     done
