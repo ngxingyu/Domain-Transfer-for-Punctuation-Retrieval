23f96e3f0f059f443036b7d187d87e8254ae9ccc
XYNG <ng.xingyu@dhs.sg>
2021-03-08 03:20:31 +0000

    fix torchlightning version error

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ source ~/.bashrc
 conda create --name NLP python=3.8
 conda activate NLP
 pip install snoop nemo_toolkit[all]==1.0.0b2 transformers datasets hydra-core git+https://github.com/pabloppp/pytorch-tools -U
-conda install -c conda-forge pytorch-lightning
+# conda install -c conda-forge pytorch-lightning=1.1.5
+pip install pytorch-lightning==1.1.5
 conda install pytorch cudatoolkit=11.1 -c pytorch #10.1
