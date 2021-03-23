e5967852f6b8f7b7982055ce38736b692ff430d4
XYNG <ng.xingyu@dhs.sg>
2021-03-23 11:30:38 +0800

    exp

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ conda create --name NLP python=3.8
 conda activate NLP
 pip install snoop nemo_toolkit[all]==1.0.0b2 transformers datasets hydra-core git+https://github.com/pabloppp/pytorch-tools -U
 # conda install -c conda-forge pytorch-lightning=1.1.5
-pip install pytorch-lightning==1.1.5
+pip install pytorch-lightning==1.1.5 albumentations
 conda install pytorch cudatoolkit=11.1 -c pytorch #10.1
